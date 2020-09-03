bf160692d5b959adbd003461a2c0657be54365b4
Huda <halacoy97@gmail.com>
2020-09-03 12:41:40 -0700

    Initial commit

---
+++ app/src/main/java/com/juaracoding/fecth/model/Clouds.java
@@ -0,0 +1,64 @@
+
+package com.juaracoding.fecth.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Clouds implements Serializable, Parcelable
+{
+
+    @SerializedName("all")
+    @Expose
+    private Integer all;
+    public final static Creator<Clouds> CREATOR = new Creator<Clouds>() {
+
+
+        @SuppressWarnings({"unchecked"})
+        public Clouds createFromParcel(Parcel in) {
+            return new Clouds(in);
+        }
+
+        public Clouds[] newArray(int size) {
+            return (new Clouds[size]);
+        }
+
+    }
+    ;
+    private final static long serialVersionUID = 2125032961067723201L;
+
+    protected Clouds(Parcel in) {
+        this.all = ((Integer) in.readValue((Integer.class.getClassLoader())));
+    }
+
+
+    public Clouds() {
+    }
+
+
+    public Clouds(Integer all) {
+        super();
+        this.all = all;
+    }
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(all);
+    }
+
+    public int describeContents() {
+        return  0;
+    }
+
+}
